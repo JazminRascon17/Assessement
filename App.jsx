888f8564dce2abc513999576079ed108d51fbe5e
Jazmin Rascon Jusaino <jazminjusaino8@gmail.com>
2022-10-10 16:36:55 -0700

    otro archivo

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulario from './Assessement/Formulario'
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario></Formulario>
+
+    </div>
+  
+  );
+}
+
+export default App;
