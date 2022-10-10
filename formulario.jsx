8d66022d549e570c8e639ba4738b4591ae210139
Jazmin Rascon Jusaino <jazminjusaino8@gmail.com>
2022-10-10 11:33:01 -0700

    agregando examen

--- formulario.jsx
+++ formulario.jsx
@@ -1,45 +1,113 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import uniqid from 'uniqid'
 
-const Formulario = () => {
+export default function Formulario(props) {
 
-    const [nombre, setNombre] = useState('')
-    const [edad, setEdad] = useState('')
+    const [Nombre, setNombre] = useState('')
+    const [Fecha, setFecha] = useState('')
+    const [Descripcion, setDescripcion] = useState('')
+    const [Lista, setLista] = useState([])
 
-    const validar = (event) =>{
-        event.preventDefault()
-        console.log('pulsado el boton')
-        if(!nombre.trim()){
-            console.log('El nombre está vacío')
-            return
+    const cambiarNombre = (e) => {
+        setNombre(e.target.value);
+        const nuevoNombre ={
+            id:uniqid(),
+            tituloNombre:Nombre
         }
-        event.preventDefault()
-        console.log('pulsado el boton')
-        if(!edad.trim()){
-            console.log('La edad está vacío')
-            return 
+        
+        setLista([...Lista,nuevoNombre])
+        
     }
+    const cambiarFecha = (e) => {
+       setFecha(e.target.value);
+       const nuevoFecha ={
+        id:uniqid(),
+        tituloFecha:Fecha
+       }
+       setLista([...Lista,nuevoFecha])
+       
+       
+       
+    }
+    const cambiarDescripcion = (e) => {
+        setDescripcion(e.target.value);
+        const nuevoDescripcion ={
+            id:uniqid(),
+            tituloDescripcion:Descripcion
+        }
+        setLista([...Lista,nuevoDescripcion])
+        
+    }
+    const enviarDatos = (e) => {
+        e.preventDefault();
+        console.log(setNombre);
 
-    return (
 
-        <div className="container">
-            <form onSubmit={validar} className="form-group"> </form>
-            <form className="form-group">
-                <input placeholder='Indroduce el nombre' 
-                className="form-control mb-3" 
-                type="text"
-                onChange={ (e) => {setNombre (e.target.value) }}
+         
+    }
+    
+
+    return(
+        <div> 
+            <div className="row">
+                <div className="col">
+                    <h2>Lista</h2>
+                </div>
+            <form onSubmit={enviarDatos}>
+                
+                <h2>Tareas</h2>
+                
+
+                <label>Nombre
+                <input className="form-control mb-3" type="text" 
+                value={Nombre} 
+                onChange={cambiarNombre}
+                placeholder="Intruduce tu nombre"
+                
                 />
-                <input placeholder='Introduce la edad' 
-                className="form-control mb-3" 
-                type="text"/>
-                <input className="btn btn-info btn-block mb-3" 
-                type="submit"
-                onChange={ (e) => {setEdad (e.target.value)}}
-                />
+                
+                </label>
+                <br/>
+                <label> Fecha 
+                <input className="form-control mb-3"type="text" 
+                     value={Fecha} 
+                     onChange={cambiarFecha}
+                     placeholder="Intruduce la fecha de entrega"
+                     />  
+                </label>
+                <br/>
+                <label> Descripcion 
+                    <input className="form-control mb-3" type="text" 
+                    value={Descripcion} 
+                    onChange={cambiarDescripcion}
+                    placeholder="descripcion de la tarea"
+                    />
+
+                </label>
+                <br/>
+
+                <button type="submit">Guardar</button>
+
+                <ul className="list-group">
+                    {
+                         Lista.map(item=>
+                            <li key={item.id} className="list-group-item"> {item.tituloNombre} </li> 
+                            )          
+                    }
+                    {
+                         Lista.map(item=>
+                            <li key={item.id} className="list-group-item"> {item.tituloFecha} </li> 
+                            )          
+                    }
+                    {
+                         Lista.map(item=>
+                            <li key={item.id} className="list-group-item"> {item.tituloDescripcion} </li> 
+                            )          
+                    }
+                </ul>
             </form>
-
+            
+            </div>
         </div>
     )
 }
-}
-export default Formulario
